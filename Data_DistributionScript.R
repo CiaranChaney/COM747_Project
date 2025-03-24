83e5ec7f50f6d983eeba870a9525dd1378b46730
Ciaran Chaney <mrcchaney@gmail.com>
2025-03-24 20:21:01 +0000

    Added normal or skewed and scatter plots

--- Data_DistributionScript.R
+++ Data_DistributionScript.R
@@ -15,9 +15,25 @@ check_distribution <- function(data) {
             geom_density(color = "red", size = 1) +
             ggtitle(paste("Distribution of", col)))
     
+    # Scatter Plot
+    print(ggplot(data, aes(x = seq_along(.data[[col]]), y = .data[[col]])) +
+            geom_point(color = "blue", alpha = 0.5) +
+            ggtitle(paste("Scatter Plot of", col)) +
+            xlab("Index") +
+            ylab(col))
+    
     # Skew
     skew <- skewness(data_numeric[[col]], na.rm = TRUE)
     cat("Skew:", skew, "\n")
+    
+    # Determine if the data is normal or skewed
+    if (abs(skew) < 0.5) {
+      cat("The data appears to be normally distributed.\n")
+    } else if (skew > 0.5) {
+      cat("The data is positively skewed (right-skewed).\n")
+    } else {
+      cat("The data is negatively skewed (left-skewed).\n")
+    }
     
   }
 }
