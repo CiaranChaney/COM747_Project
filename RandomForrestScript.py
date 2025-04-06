bc6b35b639f59d6195639500de0247ce1bbd6f67
amcg137 <52919986+amcg137@users.noreply.github.com>
2025-04-06 22:21:01 +0100

    COM-747: Adding correct paths and ROC Curve

--- RandomForrestScript.py
+++ RandomForrestScript.py
@@ -7,13 +7,27 @@ from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
-from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, ConfusionMatrixDisplay
+from sklearn.metrics import confusion_matrix, classification_report, roc_auc_score, accuracy_score, ConfusionMatrixDisplay, roc_curve, RocCurveDisplay
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
+import pathlib
 
 #Load data
-filepath1= 'C:/Users/bowes/Downloads/numerical_heart_data.csv'
-filepath2 = 'C:/Users/bowes/Downloads/numerical_stroke_data.csv'
+# If using Downloads
+# download_folder = str(os.path.join(Path.home(), "Downloads"))
+# filepath = os.path.join(download_folder,fileName)
+
+print("Starting Process")
+
+filepath_1= 'C:/Users/bowes/Downloads/numerical_heart_data.csv'
+filepath1= os.path.join(pathlib.Path(__file__).parent.resolve(), 'numerical_heart_data.csv')
+
+filepath_2 = 'C:/Users/bowes/Downloads/numerical_stroke_data.csv'
+filepath2 = os.path.join(pathlib.Path(__file__).parent.resolve(), 'numerical_stroke_data.csv')
+
+print(f"Path:1 {filepath1}")
+print(f"Path:2 {filepath2}")
 
 #Prepare dataframe for numerical_heart_data
 df = pd.read_csv(filepath1)
@@ -82,4 +96,22 @@ cm = confusion_matrix(y_test, y_pred)
 plt.figure(figsize=(10, 8))
 disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=best_model.classes_)
 disp.plot()
+
+print("TIME FOR ROC")
+fpr, tpr, thresholds = roc_curve(y_test, y_pred)
+roc_display = RocCurveDisplay(fpr=fpr, tpr=tpr).plot()
+# 4. Calculate AUC
+roc_auc = roc_auc_score(y_test, y_pred)
+print(f"AUC: {roc_auc:.2f}")
+
+# 5. Plot ROC Curve
+plt.figure(figsize=(8, 6))
+plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (AUC = {roc_auc:.2f})')
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--', label='Random guess')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate (FPR)')
+plt.ylabel('True Positive Rate (TPR)')
+plt.title('Receiver Operating Characteristic (ROC) Curve for Chosen dataset')
+plt.legend(loc="lower right")
 plt.show()
